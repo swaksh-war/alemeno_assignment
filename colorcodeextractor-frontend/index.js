04ca98007a3f0ed00af8be063d0ecd481baa9e6e
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2023-09-22 12:17:54 +0530

    cors added and api  posting properly

--- colorcodeextractor-frontend/index.js
+++ colorcodeextractor-frontend/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     const imageInput = document.querySelector("#imageInput");
+    const responseContainer = document.querySelector("#responseContainer");
     form.addEventListener("submit", function (event) {
         event.preventDefault();
         
@@ -13,10 +14,14 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .then(response => {
             if(response.ok) {
-                console.log("Image uploaded successfully")
+                resp = response.json();
+                return resp;
             } else {
                 console.error("Error uploading image")
             }
+        })
+        .then(data => {
+            responseContainer.innerHTML = JSON.stringify(data, null, 2);
         })
         .catch(error => {
             console.error("Network error : ", error)
--- colorcodeextractor/colorcodeextractor/settings.py
+++ colorcodeextractor/colorcodeextractor/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "colorcodeextractor.urls"
@@ -126,4 +128,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
-MEIDA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CORS_ALLOW_ALL_ORIGINS = True
+
